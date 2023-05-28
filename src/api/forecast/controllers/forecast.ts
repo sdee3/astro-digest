7207cb11bd7b92cb30b00754b3e56d6a237d52cb
Stefan <stefd996@gmail.com>
2023-05-28 15:17:44 +0200

    New model with data

---
+++ src/api/forecast/controllers/forecast.ts
@@ -0,0 +1,7 @@
+/**
+ * forecast controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::forecast.forecast');
---
+++ src/api/forecast/routes/forecast.ts
@@ -0,0 +1,7 @@
+/**
+ * forecast router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::forecast.forecast');
